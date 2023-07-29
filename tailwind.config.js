db2310cf8ea809c19792954e0db1cbac7f732204
fawaz27 <boukarifawaz27@gmail.com>
2023-07-29 01:16:47 +0100

    theme change

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,76 +9,148 @@ import {
   emerald,
   neutral,
 } from "tailwindcss/colors";
+
 const lightTheme = {
   backgroundColor: {
-    DEFAULT: "#f6e9e4",
-    primary: "#d8572a",
-    secondary: "#FFFFFF",
-    accent: emerald[100],
-    muted: orange[100],
+    DEFAULT: "#F8F1ED", // Light beige background
+    primary: "#E57250", // Coral primary color
+    secondary: "#FFFFFF", // White secondary color
+    accent: emerald[200], // Lighter shade of emerald as accent
+    muted: "#e5e7eb", // Lighter shade of orange as muted
 
-    input: neutral[100],
-    contrast: red[900],
+    input: "#F0F0F0", // Light gray for input backgrounds
+    contrast: red[900], // Dark red for high contrast elements
   },
   textColor: {
-    DEFAULT: "#0f0805",
-    primary: "#0f0805",
-    secondary: "#0f0805",
-    accent: "#0f0805",
-    muted: red[900],
+    DEFAULT: "#201C1A", // Dark brown text color
+    primary: "#201C1A", // Dark brown text color
+    secondary: "#201C1A", // Dark brown text color
+    accent: "#201C1A", // Dark brown text color
+    muted: red[900], // Dark red text color for muted elements
 
-    tooltip: black,
-    contrast: yellow[200],
+    tooltip: "#000000", // Black text for tooltips
+    contrast: yellow[300], // Lighter shade of yellow for contrast text
   },
   borderColor: {
-    DEFAULT: red[500],
-    tab: red[500],
-    active: red[700],
-    inactive: yellow[200],
+    //DEFAULT: red[200],    // Red border color
+    tab: red[100], // Red border color for tabs
+    active: red[700], // Darker red for active elements
+    inactive: yellow[300], // Lighter shade of yellow for inactive elements
   },
   ringColor: {
-    DEFAULT: red[400],
+    DEFAULT: red[400], // Red ring color
   },
   boxShadowColor: {
-    DEFAULT: orange[700],
+    DEFAULT: orange[500], // Orange box shadow color
   },
 };
 
 const darkTheme = {
   backgroundColor: {
-    DEFAULT: "#050505",
-    primary: "#d8572a",
-    secondary: "#061b23",
-    accent: "#70f5d6",
-    muted: "#D88D72",
+    DEFAULT: "#121212", // Dark gray background
+    primary: "#E57250", // Coral primary color
+    secondary: "#0D2A31", // Dark blue secondary color
+    accent: "#70f5d6", // Light blue as accent
+    muted: "#0D2A31", // Brownish muted color
 
-    input: neutral[900],
-    contrast: yellow[200],
+    input: "#101010", // Dark gray for input backgrounds
+    contrast: yellow[300], // Lighter shade of yellow for high contrast elements
   },
   textColor: {
-    DEFAULT: "fafafa",
-    primary: "fafafa",
-    secondary: "fafafa",
-    accent: "fafafa",
-    tertiary: "fafafa",
-    muted: "fafafa",
+    DEFAULT: "#FAFAFA", // Off-white text color
+    primary: "#FAFAFA", // Off-white text color
+    secondary: "#FAFAFA", // Off-white text color
+    accent: "#FAFAFA", // Off-white text color
+    tertiary: "#FAFAFA", // Off-white text color
+    muted: "#FAFAFA", // Off-white text color for muted elements
 
-    tooltip: black,
-    contrast: red[900],
-  },
-  boxShadowColor: {
-    DEFAULT: orange[300],
+    tooltip: "#000000", // Black text for tooltips
+    contrast: red[900], // Dark red text color for contrast elements
   },
   borderColor: {
-    DEFAULT: red[900],
-    tab: yellow[400],
-    active: yellow[200],
-    inactive: red[900],
+    //DEFAULT: red[900],    // Dark red border color
+    tab: yellow[100], // Yellow border color for tabs
+    active: yellow[300], // Lighter shade of yellow for active elements
+    inactive: red[900], // Dark red border color for inactive elements
   },
   ringColor: {
-    DEFAULT: yellow[400],
+    DEFAULT: yellow[400], // Yellow ring color
+  },
+  boxShadowColor: {
+    DEFAULT: orange[500], // Orange box shadow color
   },
 };
+
+// const lightTheme = {
+//   backgroundColor: {
+//     DEFAULT: "#f6e9e4",
+//     primary: "#d8572a",
+//     secondary: "#FFFFFF",
+//     accent: emerald[100],
+//     muted: orange[100],
+
+//     input: neutral[100],
+//     contrast: red[900],
+//   },
+//   textColor: {
+//     DEFAULT: "#0f0805",
+//     primary: "#0f0805",
+//     secondary: "#0f0805",
+//     accent: "#0f0805",
+//     muted: red[900],
+
+//     tooltip: black,
+//     contrast: yellow[200],
+//   },
+//   borderColor: {
+//     DEFAULT: red[500],
+//     tab: red[500],
+//     active: red[700],
+//     inactive: yellow[200],
+//   },
+//   ringColor: {
+//     DEFAULT: red[400],
+//   },
+//   boxShadowColor: {
+//     DEFAULT: orange[700],
+//   },
+// };
+
+// const darkTheme = {
+//   backgroundColor: {
+//     DEFAULT: "#050505",
+//     primary: "#d8572a",
+//     secondary: "#061b23",
+//     accent: "#70f5d6",
+//     muted: "#D88D72",
+
+//     input: neutral[900],
+//     contrast: yellow[200],
+//   },
+//   textColor: {
+//     DEFAULT: "fafafa",
+//     primary: "fafafa",
+//     secondary: "fafafa",
+//     accent: "fafafa",
+//     tertiary: "fafafa",
+//     muted: "fafafa",
+
+//     tooltip: black,
+//     contrast: red[900],
+//   },
+//   boxShadowColor: {
+//     DEFAULT: orange[300],
+//   },
+//   borderColor: {
+//     DEFAULT: red[900],
+//     tab: yellow[400],
+//     active: yellow[200],
+//     inactive: red[900],
+//   },
+//   ringColor: {
+//     DEFAULT: yellow[400],
+//   },
+// };
 
 /** @type {import('tailwindcss').Config} */
 export const darkMode = "class";
