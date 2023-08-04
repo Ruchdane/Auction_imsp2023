545195fb2bedfff82ab4b8bfd4d71a80e640ee80
fawaz27 <boukarifawaz27@gmail.com>
2023-08-04 02:35:05 +0100

    change  env mode

--- domain/firebase/config.ts
+++ domain/firebase/config.ts
@@ -3,36 +3,38 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore, serverTimestamp } from "firebase/firestore";
 
-console.log('proccess.en : ',process.env.VITE_REACT_APP_FIREBASE_API_KEY);
-console.log('import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN',import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN);
-console.log('import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID',import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID);
-console.log('import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET',import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET);
-console.log('import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID',import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
-console.log('import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID',import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID);
-console.log('import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID',import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID);
+// if (
+//   !import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY ||
+//   !import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN ||
+//   !import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID ||
+//   !import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET ||
+//   !import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID ||
+//   !import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID ||
+//   !import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID
+// ) {
+//   console.error(
+//     "Certaines variables d'environnement Firebase sont manquantes.",
+//   );
+// }
 
-if (
-  !import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY ||
-  !import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN ||
-  !import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID ||
-  !import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET ||
-  !import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID ||
-  !import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID ||
-  !import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID
-) {
-  console.error(
-    "Certaines variables d'environnement Firebase sont manquantes.",
-  );
-}
+// const firebaseApp = initializeApp({
+//   apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY||"",
+//   authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN || "",
+//   projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID || "",
+//   storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET || "",
+//   messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "",
+//   appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID || "",
+//   measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID || "",
+// });
 
 const firebaseApp = initializeApp({
-  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY||"",
-  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN || "",
-  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID || "",
-  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID || "",
-  measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID || "",
+  apiKey: "AIzaSyB_07JIN6bQi9_LCywQ22q2zcx5DcHcyvs",
+  authDomain: "test-3ee84.firebaseapp.com",
+  projectId: "test-3ee84",
+  storageBucket: "test-3ee84.appspot.com",
+  messagingSenderId: "663304868559",
+  appId: "1:663304868559:web:b654a57d106461dcad5dba",
+  measurementId: "G-8WB0TJL66M",
 });
 
 export const firestoreApp = getFirestore(firebaseApp);
